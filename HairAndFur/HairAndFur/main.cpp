4f45d2a6c92deaa5f6de2dc8af19b14ce5ec8ea9
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-11-10 21:56:17 +0000

    lmao

--- HairAndFur/HairAndFur/main.cpp
+++ HairAndFur/HairAndFur/main.cpp
@@ -1,158 +1,147 @@
-//Include necessary Headers//
 #include <windows.h>
 
-//Define variables/constants//
-LPCTSTR WndClassName = L"firstwindow";    //Define our window class name
-HWND hwnd = NULL;    //Sets our windows handle to NULL
+LPCTSTR WndClassName = L"firstwindow";  
+HWND hwnd = NULL; 
 
-const int Width = 800;    //window width
-const int Height = 600;    //window height
+const int Width = 800; 
+const int Height = 600;
 
-						   //Functions//
-bool InitializeWindow(HINSTANCE hInstance,    //Initialize our window
+bool InitializeWindow(HINSTANCE hInstance,  
 	int ShowWnd,
 	int width, int height,
 	bool windowed);
 
-int messageloop();    //Main part of the program
+int messageloop();  
 
-LRESULT CALLBACK WndProc(HWND hWnd,    //Windows callback procedure
+LRESULT CALLBACK WndProc(HWND hWnd,
 	UINT msg,
 	WPARAM wParam,
 	LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
+int WINAPI WinMain(HINSTANCE hInstance, 
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nShowCmd)
 {
-	//Initialize Window//
 	if (!InitializeWindow(hInstance, nShowCmd, Width, Height, true))
 	{
-		//If initialization failed, display an error message
 		MessageBox(0, L"Window Initialization - Failed",
 			L"Error", MB_OK);
 		return 0;
 	}
 
-	messageloop();    //Jump into the heart of our program
+	messageloop();    
 
 	return 0;
 }
 
-bool InitializeWindow(HINSTANCE hInstance,    //Initialize our window
+bool InitializeWindow(HINSTANCE hInstance, 
 	int ShowWnd,
 	int width, int height,
 	bool windowed)
 {
-	//Start creating the window//
 
-	WNDCLASSEX wc;    //Create a new extended windows class
+	WNDCLASSEX wc; 
 
-	wc.cbSize = sizeof(WNDCLASSEX);    //Size of our windows class
-	wc.style = CS_HREDRAW | CS_VREDRAW;    //class styles
-	wc.lpfnWndProc = WndProc;    //Default windows procedure function
-	wc.cbClsExtra = NULL;    //Extra bytes after our wc structure
-	wc.cbWndExtra = NULL;    //Extra bytes after our windows instance
-	wc.hInstance = hInstance;    //Instance to current application
-	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);    //Title bar Icon
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);    //Default mouse Icon
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);    //Window bg color
-	wc.lpszMenuName = NULL;    //Name of the menu attached to our window
-	wc.lpszClassName = WndClassName;    //Name of our windows class
-	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO); //Icon in your taskbar
+	wc.cbSize = sizeof(WNDCLASSEX);    
+	wc.style = CS_HREDRAW | CS_VREDRAW;    
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;    
+	wc.cbWndExtra = NULL;    
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO); 
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);    
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2); 
+	wc.lpszMenuName = NULL;   
+	wc.lpszClassName = WndClassName;    
+	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
 
-	if (!RegisterClassEx(&wc))    //Register our windows class
+	if (!RegisterClassEx(&wc))   
 	{
-		//if registration failed, display error
+		
 		MessageBox(NULL, L"Error registering class",
 			L"Error", MB_OK | MB_ICONERROR);
 		return 1;
 	}
 
-	hwnd = CreateWindowEx(    //Create our Extended Window
-		NULL,    //Extended style
-		WndClassName,    //Name of our windows class
-		L"Hey guys, MASSIVE LEGEND here",    //Name in the title bar of our window
-		WS_OVERLAPPEDWINDOW,    //style of our window
-		CW_USEDEFAULT, CW_USEDEFAULT,    //Top left corner of window
-		width,    //Width of our window
-		height,    //Height of our window
-		NULL,    //Handle to parent window
-		NULL,    //Handle to a Menu
-		hInstance,    //Specifies instance of current program
-		NULL    //used for an MDI client window
+	hwnd = CreateWindowEx(
+		NULL,  
+		WndClassName, 
+		L"Hey guys, MASSIVE LEGEND here", 
+		WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT,    
+		width,  
+		height, 
+		NULL,   
+		NULL,   
+		hInstance,   
+		NULL  
 		);
 
-	if (!hwnd)    //Make sure our window has been created
+	if (!hwnd)  
 	{
-		//If not, display error
 		MessageBox(NULL, L"Error creating window",
 			L"Error", MB_OK | MB_ICONERROR);
 		return 1;
 	}
 
-	ShowWindow(hwnd, ShowWnd);    //Shows our window
-	UpdateWindow(hwnd);    //Its good to update our window
+	ShowWindow(hwnd, ShowWnd);   
+	UpdateWindow(hwnd);  
 
-	return true;    //if there were no errors, return true
+	return true;   
 }
 
-int messageloop() {    //The message loop
+int messageloop() {   
 
-	MSG msg;    //Create a new message structure
-	ZeroMemory(&msg, sizeof(MSG));    //clear message structure to NULL
+	MSG msg;    
+	ZeroMemory(&msg, sizeof(MSG));   
 
-	while (true)    //while there is a message
+	while (true)   
 	{
-		//if there was a windows message
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT)    //if the message was WM_QUIT
-				break;    //Exit the message loop
+			if (msg.message == WM_QUIT)
+				break;  
 
-			TranslateMessage(&msg);    //Translate the message
-
-									   //Send the message to default windows procedure
+			TranslateMessage(&msg);    
+									   
 			DispatchMessage(&msg);
 		}
 
 		else 
-		{    //Otherewise, keep the flow going
+		{   
 
 		}
-
-
 	}
 
-	return (int)msg.wParam;        //return the message
+	return (int)msg.wParam;    
 
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd,    //Default windows procedure
+LRESULT CALLBACK WndProc(HWND hwnd,  
 	UINT msg,
 	WPARAM wParam,
 	LPARAM lParam)
 {
-	switch (msg)    //Check message
+	switch (msg)
 	{
 
-	case WM_KEYDOWN:    //For a key down
-						//if escape key was pressed, display popup box
+	case WM_KEYDOWN: 
+					
 		if (wParam == VK_ESCAPE) {
 			if (MessageBox(0, L"Are you sure you want to exit?",
 				L"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
 
-				//Release the windows allocated memory  
+				
 				DestroyWindow(hwnd);
 		}
 		return 0;
 
-	case WM_DESTROY:    //if x button in top right was pressed
+	case WM_DESTROY:  
 		PostQuitMessage(0);
 		return 0;
 	}
-	//return the message for windows to handle it
+
 	return DefWindowProc(hwnd,
 		msg,
 		wParam,
